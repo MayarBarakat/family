913765566d914433036fa7007800ef310338dd2c
Your Name <you@example.com>
2023-03-14 12:25:17 +0300

    sql2

--- src/main/java/com/example/familybackend/FamilyBackendApplication.java
+++ src/main/java/com/example/familybackend/FamilyBackendApplication.java
@@ -2,8 +2,9 @@ package com.example.familybackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class FamilyBackendApplication {
 
 	public static void main(String[] args) {
